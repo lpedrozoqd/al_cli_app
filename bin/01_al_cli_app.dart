37ff5916a95e96b8105a4360eef5092571c16559
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-05-25 22:10:31 -0500

    v250522.2210 - modificaciones varias

--- bin/01_al_cli_app.dart
+++ bin/01_al_cli_app.dart
@@ -1,4 +1,4 @@
-import 'package:al_cli_app/hola_mundo.dart' as fnc;
+import 'package:al_cli_app/01_hola_mundo.dart' as fnc;
 
 void main(List<String> arguments) {
   print('Hello world!:${fnc.calculate()}');
---
+++ bin/02_variables.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  int varEntera = 10;
+  double varDouble = 3.1416;
+  String varCadena = "Dart es cool!";
+  bool varBoolean = true;
+  varBoolean = false;
+  var varAlgo = 10;
+  dynamic varDynamic = 10;
+  varDynamic = varAlgo;
+  varDynamic = varBoolean;
+
+  print('Datos entero: $varEntera');
+  print('Dato dobule: $varDouble');
+  print('Dato cadena: $varCadena');
+  print('Dato booleno: $varBoolean');
+  print('Dato dynamic: $varDynamic');
+}
